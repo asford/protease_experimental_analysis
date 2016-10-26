c3e0884e2418e28440ce293c52b7f582af82eb8d
Alex Ford <fordas@uw.edu>
2016-10-25 17:55:55 -0700

    Update example notebook with data submodule, add decoy names to data.

--- data/__init__.py
+++ data/__init__.py
@@ -7,7 +7,10 @@ import glob
 import os
 from os import path
 
+import collections
+
 __all__ = ["model_input"]
+
 basedir = path.dirname(__file__) 
 
 def none_or_int(x):
@@ -25,31 +28,39 @@ def fix_num_selected(x):
         return 5e5
     return x
 
-df=pd.read_csv(path.join(basedir, 'experiments.csv'))
-df=df.dropna(how='all')
-df=df.fillna('-')
-df=df.where( df != '-', None)
+summary=pd.read_csv(path.join(basedir, 'experiments.csv'))
+summary=summary.dropna(how='all')
+summary=summary.fillna('-')
+summary=summary.where( summary != '-', None)
 
-alldata={}
+model_input = {}
 
-for i in range(len(df)):
-    if df['input'].values[i] not in alldata:
-        alldata[df['input'].values[i]] = {}
+for i, r in summary.iterrows():
+    name = r["input"].replace(".counts", "")
+    if name not in model_input:
+        model_input[name] = {}
 
-    alldata[df['input'].values[i]][int(df['column'].values[i].replace('counts',''))] = dict(
-        parent = df['parent'].values[i],
-        selection_level=df['selection_strength'].values[i],
-        num_selected=fix_num_selected(df['cells_collected'].values[i]),
-        fraction_selected=none_or_div(df['fraction_collected'].values[i], df['parent_expression'].values[i]),
-        conc_factor=df['conc_factor'].values[i]
+    rnd = int(r["column"].replace("counts", ""))
+    assert rnd not in model_input[name]
+
+    model_input[name][rnd] = dict(
+        parent            = r['parent'],
+        selection_level   = r['selection_strength'],
+        num_selected      = fix_num_selected(r['cells_collected']),
+        fraction_selected = none_or_div(r['fraction_collected'], r['parent_expression']),
+        conc_factor       = r['conc_factor']
     )
 
-for exper in alldata:
-    counts_df = pd.read_csv(path.join(basedir, exper),delim_whitespace=True)
+for exper in model_input:
+    counts_df = pd.read_csv(
+            path.join(basedir, exper + ".counts"),
+            delim_whitespace=True)
+
     for i, col in enumerate(counts_df.columns[1:]):
-        alldata[exper][i]['seq_counts'] = counts_df[col].astype(np.int)
+        model_input[exper][i]["name"] = counts_df["name"]
+        model_input[exper][i]['seq_counts'] = counts_df[col].astype(np.int)
     
-    for k, v in alldata[exper].items():
+    for k, v in model_input[exper].items():
         if v["seq_counts"].sum() > v["num_selected"]: 
             pfrac = v["seq_counts"].values.astype(float) / v['seq_counts'].sum()
             v["selected"] = np.floor(pfrac * v["num_selected"])
@@ -57,5 +68,3 @@ for exper in alldata:
             v["num_selected"] = v["seq_counts"].sum()
             v["selected"] = np.array(v["seq_counts"].astype(np.float))
         v['min_fraction'] = None
-
-model_input = { k.replace(".counts", "") : v for k, v in alldata.items() }
